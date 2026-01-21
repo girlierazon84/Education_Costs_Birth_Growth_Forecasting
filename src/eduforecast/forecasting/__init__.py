ba2b13ac77d06e084e02e7d91377d933ade1a3f0
Girlie Razon <girlie.razon84@gmail.com>
2026-01-21 20:27:08 +0100

    feat(forecasting): expose public forecasting API in package __init__

--- src/eduforecast/forecasting/__init__.py
+++ src/eduforecast/forecasting/__init__.py
@@ -1,5 +1,16 @@
-"""
-src/eduforecast/forecasting/__init__.py
+"""src/eduforecast/forecasting/__init__.py"""
 
-Forecasting helpers (births, population, intervals).
-"""
+from .intervals import IntervalResult, empirical_pi, normal_pi
+from .predict_births import predict_births_all_regions, predict_births_for_region
+from .predict_mortality import predict_mortality_stub
+from .predict_population import predict_population_0_19
+
+__all__ = [
+    "IntervalResult",
+    "normal_pi",
+    "empirical_pi",
+    "predict_births_for_region",
+    "predict_births_all_regions",
+    "predict_population_0_19",
+    "predict_mortality_stub",
+]
